309db4c14d72beea4677be40de9fc7d4532b785a
toka <t.abdelhamied@egyptsmartcards.com>
2022-10-20 13:18:10 +0200

    integrate and import remote products files in the container

--- products/src/index.js
+++ products/src/index.js
@@ -1,10 +1,11 @@
 import faker from "faker";
 
-let products = "";
+let products = "<H2>this is products</H2>";
 
 for (let i = 0; i < 5; i++) {
   //   console.log(faker.commerce.productName());
   const name = faker.commerce.productName();
   products += `<div>${name}</div>`;
 }
+
 document.querySelector("#dev-products").innerHTML = products;
